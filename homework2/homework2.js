a2f26814793f3cdb77c4459d6584c9ee968fafe7
onqqr <dnbyaniss@gmail.com>
2024-06-02 14:15:11 +0300

    fix: fix comments

--- homework2/homework2.js
+++ homework2/homework2.js
@@ -8,11 +8,11 @@ for (var i = 0; i < 5; i++) {
   }
   })(i)
 }
-result[0](); //5
-result[1](); //5
-result[2](); //5
-result[3](); //5
-result[4](); //5
+result[0](); //0
+result[1](); //1
+result[2](); //2
+result[3](); //3
+result[4](); //4
 
 //////////////////////////////////////////////////
 
@@ -33,8 +33,8 @@ function getGroup() {
 
 let group = getGroup();
 
-group[0](); // 10 как исправить на 0
-group[5](); // 10                  5
+group[0](); // 0
+group[5](); // 5                 5
 
 //////////////////////////////////////////////////
 
@@ -54,18 +54,18 @@ function multiply(x) {
 }
 
 const result1 = multiply(2)(3)(4)(8)();
-console.log(result1); // Вывод: 24
+console.log(result1);
 const result2 = multiply(2)(3)(4)(5)();
-console.log(result2); // Вывод: 120
+console.log(result2);
 
 // const result1 = multiply(2)(3)(4);
 
 // // Пример использования:
-const result3 = multiply(2)(4)();
-console.log(result1); // Вывод: 8
+// const result3 = multiply(2)(4)();
+// console.log(result1);
 
-const result4 = multiply(5)(2)(3)();
-console.log(result2); // Вывод: 30
+// const result4 = multiply(5)(2)(3)();
+// console.log(result2);
 
 /////////////////////////
 // Написать функцию getUniqArray(arr), которая на вход принимает массив чисел и
@@ -88,5 +88,5 @@ function getUniqArray(arr) {
 // const arr2 = [1, 2, '3', 5, 5, 6, 7, '7']
 // getUniqArray(arr2)
 
-const arr3 = [1, 1, 1, 1, 1, 34]
+const arr3 = [1, 1, 1, 34]
 getUniqArray(arr3)
